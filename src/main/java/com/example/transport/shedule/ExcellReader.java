c7163d597e05de55938214dde54cb14af21f90cc
Aleksei Gorshkov <alexeidmitrich@gmail.com>
2022-08-06 10:02:10 +0300

    divided classes with reading

--- src/main/java/com/example/transport/shedule/ExcellReader.java
+++ src/main/java/com/example/transport/shedule/ExcellReader.java
@@ -1,40 +1,12 @@
 package com.example.transport.shedule;
 
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.*;
-
 import java.io.*;
-import java.util.Iterator;
 import jxl.Cell;
 import jxl.Sheet;
 import jxl.Workbook;
 public class ExcellReader {
     public static void main(String[] args) throws Exception {
-        /*try {
-            FileInputStream excelFile = new FileInputStream("C:\\Users\\alexe\\Downloads\\Timetable.xls");
-            Workbook wb = new HSSFWorkbook(excelFile);
-            Sheet datatypeSheet = wb.getSheetAt(2);
-            Iterator<Row> iterator = datatypeSheet.iterator();
-            while (iterator.hasNext()) {
-                Row currentRow = iterator.next();
-                Iterator<Cell> cellIterator = currentRow.iterator();
-                while (cellIterator.hasNext()) {
-                    Cell currentCell = cellIterator.next();
-                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
-                        System.out.print(currentCell.getStringCellValue() + "--");
-                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
-                        System.out.print(currentCell.getNumericCellValue() + "--");
-                    }
-                }
-                System.out.println();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
         File f = new File("C:\\Users\\alexe\\Downloads\\Timetable.xls");
-
         try {
             Workbook wb=Workbook.getWorkbook(f);
             int Num = wb.getNumberOfSheets();
@@ -57,7 +29,6 @@ public class ExcellReader {
         } catch (Exception e) {
             e.getMessage();
         }
-
         System.out.println("completed");
     }
 }
---
+++ src/main/java/com/example/transport/shedule/ExellReaderOnePage.java
@@ -0,0 +1,36 @@
+package com.example.transport.shedule;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import java.io.*;
+import java.util.Iterator;
+
+import java.io.FileInputStream;
+
+public class ExellReaderOnePage {
+    public static void main(String[] args) throws Exception {
+        try {
+            FileInputStream excelFile = new FileInputStream("C:\\Users\\alexe\\Downloads\\Timetable.xls");
+            Workbook wb = new HSSFWorkbook(excelFile);
+            Sheet datatypeSheet = wb.getSheetAt(1);
+            Iterator<Row> iterator = datatypeSheet.iterator();
+            while (iterator.hasNext()) {
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
+                while (cellIterator.hasNext()) {
+                    Cell currentCell = cellIterator.next();
+                    if (currentCell.getCellTypeEnum() == CellType.STRING) {
+                        System.out.print(currentCell.getStringCellValue() + "--");
+                    } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+                        System.out.print(currentCell.getNumericCellValue() + "--");
+                    }
+                }
+                System.out.println();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
