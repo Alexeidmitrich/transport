3f8ac471881d9a2e097919ecabc33472ab6f73b4
Aleksei Gorshkov <alexeidmitrich@gmail.com>
2022-08-06 09:33:55 +0300

    Another way to read the file

--- src/main/java/com/example/transport/shedule/ExcellReader.java
+++ src/main/java/com/example/transport/shedule/ExcellReader.java
@@ -5,10 +5,12 @@ import org.apache.poi.ss.usermodel.*;
 
 import java.io.*;
 import java.util.Iterator;
-
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
 public class ExcellReader {
-    public static void main(String[] args) {
-        try {
+    public static void main(String[] args) throws Exception {
+        /*try {
             FileInputStream excelFile = new FileInputStream("C:\\Users\\alexe\\Downloads\\Timetable.xls");
             Workbook wb = new HSSFWorkbook(excelFile);
             Sheet datatypeSheet = wb.getSheetAt(2);
@@ -30,7 +32,33 @@ public class ExcellReader {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }*/
+        File f = new File("C:\\Users\\alexe\\Downloads\\Timetable.xls");
+
+        try {
+            Workbook wb=Workbook.getWorkbook(f);
+            int Num = wb.getNumberOfSheets();
+
+            for (int i = 0; i < Num; i++) {
+                Sheet sheet = wb.getSheet(i);
+                Sheet s = wb.getSheet(i);
+
+                int row = s.getRows();
+                int col = s.getColumns();
+
+                for(int i1=0; i1<row;i1++) {
+                    for(int j=0;j<col;j++) {
+                        Cell c =s.getCell(j, i1);
+                        System.out.println(c.getContents());
+                        //String text = c.getContents(); 
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.getMessage();
         }
+
+        System.out.println("completed");
     }
 }
 
