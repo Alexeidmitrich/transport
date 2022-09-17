bf91f916507820088bb25b6d4361a5bb335bd554
Aleksei Gorshkov <alexeidmitrich@gmail.com>
2022-09-17 14:32:20 +0300

    Restored the getJourneyTest method in the class ExcelReaderTest

--- src/test/java/com/example/transport/shedule/ExcelReaderTest.java
+++ src/test/java/com/example/transport/shedule/ExcelReaderTest.java
@@ -7,7 +7,7 @@ import com.example.transport.domain.StopTransport;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.util.ArrayList;
+import java.time.LocalTime;
 import java.util.List;
 import java.util.Map;
 
@@ -25,24 +25,26 @@ class ExcelReaderTest {
         assertEquals(3, journeyList.size());
         testList(journeyList, 0, 4);
         testList(journeyList, 1, 4);
-        testList(journeyList,2,4);
+        testList(journeyList, 2, 4);
         List<Journey> journeyInnerFirstList = journeyList.get(0);
         Journey firstJourney = journeyInnerFirstList.get(0);
         assertEquals(7, firstJourney.getJourneyStops().size());
+        String[] expectedTime = {"04:59", "05:03", "05:04", "05:05", "05:07", "05:09", "05:10"};
+        String[] stopTransport = {"ОСТ1000001", "ОСТ1000002", "ОСТ1000009", "ОСТ1000012", "ОСТ1000013", "ОСТ1000018", "ОСТ1000021"};
         for (int i = 0; i < firstJourney.getJourneyStops().size(); i++) {
             JourneyStop journeyStop = firstJourney.getJourneyStops().get(i);
             assertEquals("894", journeyStop.getTransport().getId());
-            Person driver =  journeyStop.getDriver();
-            assertEquals("КИН05600-2002",driver.getId());
+            Person driver = journeyStop.getDriver();
+            assertEquals("КИН05600-2002", driver.getId());
             assertEquals("Костомаров Игорь Николаевич", driver.getFio());
             Person inspector = journeyStop.getInspector();
             assertEquals("НДД92345-2009", inspector.getId());
             assertEquals("Ногакин Дмитрий Дмитриевич", inspector.getFio());
+            LocalTime timeJourney = journeyStop.getTime();
+            assertEquals(expectedTime[i], timeJourney.toString());
+            StopTransport stop = journeyStop.getStop();
+            assertEquals(stopTransport[i],stop.getId());
         }
-        List<JourneyStop> stops = journeyInnerFirstList.get(0).getJourneyStops();
-        //JourneyStop stop = stops.get(0);
-        assertEquals(7,stops.size());
-
     }
 
     @Test
@@ -53,8 +55,8 @@ class ExcelReaderTest {
         List<List<Journey>> journeys = excelReader.getJourney();
         assertEquals(0, journeys.size());
         //TODO
-        //Map<String, Person> persons = excelReader.getEmployee();
-        //assertEquals(0, persons.size());
+        Map<String, Person> personMap = excelReader.getEmployee();
+        assertEquals(0, personMap.size());
     }
 
     private void testList( List<List<Journey>> journeyList,int index, int expectedSize) {
