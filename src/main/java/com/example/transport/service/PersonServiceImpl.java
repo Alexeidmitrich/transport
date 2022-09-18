8a4b8b833cd326ceac2083233e6ba3493f684eb7
Dmitry Savkin <dimsaz@mail.ru>
2022-09-18 12:40:19 +0200

    added new method

--- src/main/java/com/example/transport/service/PersonServiceImpl.java
+++ src/main/java/com/example/transport/service/PersonServiceImpl.java
@@ -1,9 +1,12 @@
 package com.example.transport.service;
 
 import com.example.transport.domain.Person;
+import com.example.transport.exception.PersonNotFoundException;
 import com.example.transport.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PersonServiceImpl {
@@ -11,7 +14,12 @@ public class PersonServiceImpl {
     @Autowired
     private PersonRepository personRepository;
 
+    public List<Person> getPersons() {
+        return personRepository.findAll();
+    }
+
+
     public Person getPersonById(String id) {
-        return personRepository.findById(id).get();
+        return personRepository.findById(id).orElseThrow(() -> new PersonNotFoundException("Person with id " + id + " is not exists"));
     }
 }
