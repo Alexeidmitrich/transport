dcb6ab737bd7b8528bfd1299c73929c480d87206
Dmitry Savkin <dimsaz@mail.ru>
2022-11-06 19:04:47 +0100

    test-c

--- src/test/java/com/example/transport/PersonApplicationTest.java
+++ src/test/java/com/example/transport/PersonApplicationTest.java
@@ -1,10 +1,55 @@
 package com.example.transport;
 
-import org.assertj.core.internal.bytebuddy.utility.dispatcher.JavaDispatcher;
+import com.example.transport.repository.PersonRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+@ContextConfiguration(initializers = {PersonApplicationTest.Initializer.class})
 public class PersonApplicationTest {
 
-    //https://blog.sandra-parsick.de/2020/05/21/using-testcontainers-in-spring-boot-tests-for-database-integration-tests/
+    @Container
+    private static MySQLContainer mySQLContainer = new MySQLContainer("mysql:latest");
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + mySQLContainer.getUsername(),
+                    "spring.datasource.password=" + mySQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    @Test
+    public void test() {
+        System.out.println(mySQLContainer.getJdbcUrl());
+        System.out.println(mySQLContainer.getDatabaseName());
+        System.out.println(mySQLContainer.getUsername());
+        System.out.println(mySQLContainer.getPassword());
+        System.out.println("=================0");
+        System.out.println();
+        Assertions.assertEquals(2, 2);
+        Assertions.assertEquals(0, personRepository.findAll().size());
+    }
 }
